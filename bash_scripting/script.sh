9a67641a1873e1fb41ebf3aee97c4a4915b649c1
monupanwar1 <monupanwar5655@gmail.com>
2025-11-21 22:44:30 +0530

    bash_scripting

---
+++ bash_scripting/script.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# variable
+
+# name="KunalPanwar"
+# echo "Welcome,${name}"
+
+# if_else
+# name="JohnDoe"
+# if [ "$name" == "JohnDoe" ];then
+# echo "you are ${name}"
+# else 
+#   "you are not ${name}"
+# fi
+
+
+# Single Quotes vs Double Quotes
+# name='KunalPanwar'
+# echo "Welcome ,${name}"
+
+
+# loops
+
+# for num in 1 2 3 4 5; do
+# echo $num
+#   sleep 0.5
+#    done
+
+# for num in {1..100}; do
+# echo "$num"
+#   sleep 0.5
+#    done
+
+
+
+# lists
+
+# languages=("java" "Golang" "javascript")
+
+
+# for items in "${languages[@]}"; do
+# echo "$items"
+# done
+
+
+# for ((i=0; i<"${#languages[@]}" ;i++)); do
+# echo "${#languages[i]}"
+# done
+
+# Program Arguments
+# echo "$1"
+
